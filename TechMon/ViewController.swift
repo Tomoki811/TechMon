20118d18b3acec02f918c4c8c7ce8cfc2b604cc5
Tomoki Nishino <tomokinishinojp@yahoo.co.jp>
2020-06-19 17:59:29 +0900

    Initial Commit

---
+++ TechMon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechMon
+//
+//  Created by 西野智樹 on 2020/06/19.
+//  Copyright © 2020 Nishino Tomoki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
